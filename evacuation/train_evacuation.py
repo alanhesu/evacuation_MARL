77327863398f05e7ae0fa88e0256def3b1dd9e7b
Alan Hesu <alanhesu@gmail.com>
2022-04-29 17:07:20 -0400

    add monitor reward callback

--- evacuation/train_evacuation.py
+++ evacuation/train_evacuation.py
@@ -1,18 +1,34 @@
+import os
+import gym
+import numpy as np
+import matplotlib.pyplot as plt
+
 from stable_baselines3.ppo import CnnPolicy
 from stable_baselines3 import PPO, A2C, DQN
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common import results_plotter
 from pettingzoo.butterfly import knights_archers_zombies_v9, pistonball_v6
 import supersuit as ss
 import evacuation_v1
+from callbacks import SaveOnBestTrainingRewardCallback
 
+log_dir = './log'
+timesteps = 2e6
 env = evacuation_v1.parallel_env(despawn=False)
+env = Monitor(env, log_dir)
 # env = pistonball_v6.env()
 env = ss.black_death_v2(env)
 env = ss.pettingzoo_env_to_vec_env_v0(env)
 env = ss.concat_vec_envs_v0(env, 16, num_cpus=1, base_class='stable_baselines3')
 # model = PPO('MlpPolicy', env, verbose=3, learning_rate=1e-4, n_steps=2048, batch_size=4096, tensorboard_log='./log/')
-model = DQN('MlpPolicy', env, verbose=3, learning_rate=1e-4, batch_size=4096, tensorboard_log='./log/')
-model.learn(total_timesteps=1e6)
+model = DQN('MlpPolicy', env, verbose=3, learning_rate=1e-4, batch_size=4096, tensorboard_log=log_dir, gradient_steps=-1)
+
+callback = SaveOnBestTrainingRewardCallback(check_freq=1000, log_dir=log_dir)
+model.learn(total_timesteps=timesteps, callback=callback)
 model.save("evac_policy1")
+
+plot_results([log_dir], timesteps, results_plotter.X_TIMESTEPS, "policy")
+plt.show()
 
 # Rendering
 
