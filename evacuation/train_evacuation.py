144ea294fb4d390c79a75bf8372ef253d9b0117c
Nathaniel.Belles <Nathaniel.Belles@me.com>
2022-04-30 19:56:24 -0400

    Remove import errors

--- evacuation/train_evacuation.py
+++ evacuation/train_evacuation.py
@@ -6,22 +6,31 @@ import matplotlib.pyplot as plt
 from stable_baselines3.ppo import CnnPolicy
 from stable_baselines3 import PPO, A2C, DQN
 from stable_baselines3.common.vec_env import VecMonitor
-from pettingzoo.butterfly import knights_archers_zombies_v9, pistonball_v6
+
+# from pettingzoo.butterfly import knights_archers_zombies_v9, pistonball_v6
 from pettingzoo.utils import average_total_reward
 import supersuit as ss
 import evacuation_v1
+
 # from callbacks import SaveOnBestTrainingRewardCallback
 
-log_dir = './log'
+log_dir = "./log"
 timesteps = 5e5
 env = evacuation_v1.parallel_env(despawn=False)
 env = ss.black_death_v2(env)
 env = ss.pettingzoo_env_to_vec_env_v0(env)
-env = ss.concat_vec_envs_v0(env, 4, num_cpus=1, base_class='stable_baselines3')
+env = ss.concat_vec_envs_v0(env, 4, num_cpus=1, base_class="stable_baselines3")
 env = VecMonitor(env, log_dir)
 # model = PPO('MlpPolicy', env, verbose=3, learning_rate=1e-4, n_steps=2048, batch_size=256, tensorboard_log=log_dir)
-model = DQN('MlpPolicy', env, verbose=3, learning_rate=1e-4, batch_size=4096, tensorboard_log='./log/')
+model = DQN(
+    "MlpPolicy",
+    env,
+    verbose=3,
+    learning_rate=1e-4,
+    batch_size=4096,
+    tensorboard_log="./log/",
+)
 model.learn(total_timesteps=timesteps)
-model.save("evac_policy1")
+model.save("evac_policy5")
 
-print('done')
+print("done")
