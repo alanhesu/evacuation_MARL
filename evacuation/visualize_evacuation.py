c19bccf262f2bee9f45b9a736efbfbd6fbfe1ce1
Nathaniel.Belles <Nathaniel.Belles@me.com>
2022-04-30 15:28:12 -0400

    Formatting

--- evacuation/visualize_evacuation.py
+++ evacuation/visualize_evacuation.py
@@ -11,7 +11,7 @@ env = evacuation_v1.env(despawn=False)
 # env = ss.resize_v0(env, x_size=84, y_size=84)
 # env = ss.frame_stack_v1(env, 3)
 
-model = DQN.load("evac_policy1")
+model = DQN.load("evac_policy1_good")
 
 all_steps = []
 for i in range(0, 10):
@@ -21,16 +21,16 @@ for i in range(0, 10):
         obs, reward, done, info = env.last()
         act = model.predict(obs, deterministic=True)[0] if not done else None
         env.step(act)
-        if (reward == -1):
+        if reward == -1:
             print(reward, act)
         env.render()
-        time.sleep(.05)
-        if (not done):
+        time.sleep(0.05)
+        if not done:
             steps += 1
-        if (all(value for value in env.dones.values())):
+        if all(value for value in env.dones.values()):
             break
 
     all_steps.append(steps)
-    print('steps: {}'.format(steps))
-print('average steps: {}'.format(np.mean(all_steps)))
-print('done')
+    print("steps: {}".format(steps))
+print("average steps: {}".format(np.mean(all_steps)))
+print("done")
