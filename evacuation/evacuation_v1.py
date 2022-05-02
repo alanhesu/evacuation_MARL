7f09af03a7ae4dc61aa1344aa532cf0554ec20ea
Nathaniel.Belles <Nathaniel.Belles@me.com>
2022-05-01 23:24:41 -0400

    Moving weights

--- evacuation/evacuation_v1.py
+++ evacuation/evacuation_v1.py
@@ -240,7 +240,7 @@ class Robot:
         if space[tuple(newpos.astype(int))] != Objects.EMPTY:
             # check collision
             if space[tuple(newpos.astype(int))] == Objects.EXIT:
-                w_exit = 0
+                w_exit = 10
                 # print('exit')
                 space[tuple(self.position.astype(int))] = Objects.EMPTY
                 done = True
